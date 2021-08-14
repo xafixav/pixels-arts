a2eef23c04b52f15988ebbf33724b12938406be5
xafixav <xafiworld@gmail.com>
2021-08-13 22:36:15 -0300

    requisito 2 e 3

---
+++ script.js
@@ -0,0 +1,3 @@
+/* criar a lousa de pixels com JavaScript
+ela precisa ter 25 blocos (5x(colunas), 5y(section) [5x5])
+Criar 5 blocos por coluna, ao fim da coluna criar uma secao e adicionar a proxima coluna na proxima secao */
