57455dacc69eaa37c5d1b382fa0ff7dea0470b94
xafixav <xafiworld@gmail.com>
2021-08-15 19:16:27 -0300

    requisito 4 e 5

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
-/* criar a lousa de pixels com JavaScript
-ela precisa ter 25 blocos (5x(colunas), 5y(section) [5x5])
-Criar 5 blocos por coluna, ao fim da coluna criar uma secao e adicionar a proxima coluna na proxima secao */
+ const linhaLousa = 5;
+const colunaLousa = 5;
+
+function createLousa(element, desiredClass, father) { 
+  let box = document.createElement(element);
+  document.getElementById(father).appendChild(box);  
+  box.className = desiredClass;
+}
+
+/* function preencheLousa() {
+  for (let preencheColuna = 0; preencheColuna < colunaLousa; preencheColuna += 1)
+} */
+function giveClass(element, desiredClass) {
+  element.className = desiredClass;
+}
+
+/* createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('br', 'firstPixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('br', 'firstPixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel');
+createLousa('div', 'pixelScreen', 'lousaPixel'); */
+
+
+
+
+ for (let preencheColuna = 0; preencheColuna < colunaLousa; preencheColuna += 1) {
+   
+
+  for (let preencheLinha = 1; preencheLinha <= linhaLousa; preencheLinha += 1) {
+    createLousa('div', 'pixel', 'pixel-board'); 
+  }
+  createLousa('br', 'firstPixelScreen', 'pixel-board');
+}  
