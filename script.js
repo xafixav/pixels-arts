d632b4be77ccdc857f71a1bcfb9a8a47115be526
Xafixav <xafiworld@gmail.com>
2022-03-31 00:37:12 -0300

    :white_check_mark: req 10-11-12

--- script.js
+++ script.js
@@ -1,47 +1,95 @@
-const linhaLousa = 5;
-const colunaLousa = 5;
+/* eslint-disable editorconfig/editorconfig */
+let linhaLousa = 5;
+let colunaLousa = 5;
 
-let apagaTudo = document.getElementById('clear-board')
-let getBlack = document.getElementById('preto');
-let getBlue = document.getElementById('azul');
-let getRed = document.getElementById('vermelho');
-let getGreen = document.getElementById('verde');
+const apagaTudo = document.getElementById('clear-board');
+const getBlack = document.getElementById('preto');
 const allColor = document.querySelectorAll('.color');
 window.onload = getBlack.classList.add('selected');
-let getPixels = document.getElementsByClassName('pixel');
+const getPixels = document.getElementsByClassName('pixel');
 let corSelecionada = window.getComputedStyle(getBlack).backgroundColor;
+const generateBoard = document.querySelector('#generate-board');
+const pixelBoardId = '#pixel-board';
 
-function createLousa(element, desiredClass, father) { 
-  let box = document.createElement(element);
-  document.getElementById(father).appendChild(box);
+const selectColor = (event) => {
+  const { target } = event;
+  target.style.backgroundColor = corSelecionada;
+};
+
+const randomNumber = () => Number((Math.random() * 255).toFixed());
+
+const generateRandomColor = () => `${randomNumber()}, ${randomNumber()}, ${randomNumber()}`;
+
+document.getElementById('azul').style.backgroundColor = `rgb(${generateRandomColor()})`;
+document.getElementById('vermelho').style.backgroundColor = `rgb(${generateRandomColor()})`;
+document.getElementById('verde').style.backgroundColor = `rgb(${generateRandomColor()})`;
+
+function createLousa(element, desiredClass, father) {
+  const box = document.createElement(element);
+  document.querySelector(father).appendChild(box);
   box.className = desiredClass;
-  box.addEventListener('click', function(event) {
-  event.target.style.backgroundColor = corSelecionada;
-   });
+  box.addEventListener('click', selectColor);
 }
 
-apagaTudo.addEventListener('click', function cleaner() {
-for (limpaPixel = 0 ; limpaPixel < getPixels.length; limpaPixel += 1) {
-  getPixels[limpaPixel].style.backgroundColor = "white"
+function createElement(element, desiredClass, father) {
+  const box = document.createElement(element);
+  document.querySelector(father).appendChild(box);
+  box.id = desiredClass;
 }
+
+apagaTudo.addEventListener('click', () => {
+  for (let limpaPixel = 0; limpaPixel < getPixels.length; limpaPixel += 1) {
+    getPixels[limpaPixel].style.backgroundColor = 'white';
+  }
 });
 
-
-for (let preencheColuna = 0; preencheColuna < colunaLousa; preencheColuna += 1) {
-  for (let preencheLinha = 1; preencheLinha <= linhaLousa; preencheLinha += 1) {
-    createLousa('div', 'pixel', 'pixel-board');
+const createPixelBoard = () => {
+  for (let preencheColuna = 0; preencheColuna < colunaLousa; preencheColuna += 1) {
+    for (let preencheLinha = 1; preencheLinha <= linhaLousa; preencheLinha += 1) {
+      createLousa('div', 'pixel', pixelBoardId);
+    }
+    createLousa('br', 'quebraLinha', pixelBoardId);
   }
-  createLousa('br', 'quebraLinha', 'pixel-board');
-}
+};
+
+createPixelBoard();
+
+const setSelected = (element) => {
+  const { target } = element;
+  corSelecionada = window.getComputedStyle(target).backgroundColor;
+  const getSelected = document.querySelector('.selected');
+  if (getSelected.classList.contains('selected')) {
+    getSelected.classList.remove('selected');
+  }
+  element.target.classList.add('selected');
+};
 
 for (let changeColor = 0; changeColor < allColor.length; changeColor += 1) {
-  allColor[changeColor].addEventListener('click', function setColor(element) {
-    let target = element.target
-    corSelecionada = window.getComputedStyle(target).backgroundColor;    
-    let getSelected = document.querySelector('.selected');
-  if (getSelected.classList.contains("selected")) {
-    getSelected.classList.remove("selected");
-  } 
-    element.target.classList.add('selected')
-  });
+  allColor[changeColor].addEventListener('click', setSelected);
 }
+
+const generateBoardFunc = () => {
+  const inputValue = document.querySelector('#board-size').value;
+  let value = inputValue;
+  const undefinedValueMessage = 'Board inválido!';
+  if (inputValue === '') {
+    return alert(undefinedValueMessage);
+  }
+  if (inputValue < 5) {
+    value = 5;
+  }
+  if (inputValue > 50) {
+    value = 50;
+  }
+  linhaLousa = value;
+  colunaLousa = value;
+  document.querySelector('#pixel-board').remove();
+  createElement('section', 'pixel-board', 'main');
+  createPixelBoard();
+};
+
+const addFunctions = () => {
+  generateBoard.addEventListener('click', generateBoardFunc);
+};
+
+addFunctions();
